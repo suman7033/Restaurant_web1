0e06b966c25db13d24e99c4590d3848a29e5ad4c
suman <sumobarnwal@gmail.com>
2023-09-19 20:39:25 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 function App() {
   return (
     <div>
-      <h2>Hey guys i'm here</h2>
-      <h2>first time commit</h2>
-      <h1>hello world</h1>
-      <h1>something</h1>
+      <h2>Hello world</h2>
     </div>
   );
 }
